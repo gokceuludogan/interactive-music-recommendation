ba1e99eef913dcd93b6ddea2ecf677bdf6f6e296
gokce <gokceuludogan@gmail.com>
2018-12-25 18:50:28 +0300

    data preprocessing and model descriptions

---
+++ data-preprocessing.sh
@@ -0,0 +1,16 @@
+awk -F $'\t' '{print $5}' userid-timestamp-artid-artname-traid-traname.tsv | sort -n | uniq | wc -l => number of uniq trackid: 961417
+
+awk -F $'\t' '{ print $4 ";" $5 ";"$6}' userid-timestamp-artid-artname-traid-traname.tsv | sort -n | uniq -c > lastfm_uniq_songs.csv
+
+awk ' $1 > 9 {print}' lastfm_uniq_songs.csv > lastfm_uniq_songs_ge9.csv
+
+cut -c 9- lastfm_uniq_songs_ge9.csv > lastfm_uniq_songs_ge9_wout_c.csv 
+
+awk -F\; 'NF==3 {print}' lastfm_uniq_songs_ge9_wout_c.csv > lastfm_uniq_songs_ge9_wout_c_clean.csv
+
+
+vi lastfm_uniq_songs_ge9_wout_c_clean.csv
+
+:%s/"//g
+:w lastfm_uniq_songs_ge9_wout_c_clean_wout_quotes.csv
+
